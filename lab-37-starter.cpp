d5e123b763aa59e9bda7e6cb2a4dbf3434df65e4
Kai-Cheng Mei <kennymei47@gmail.com>
2024-12-08 06:59:57 -0800

    update3

--- lab-37-starter.cpp
+++ lab-37-starter.cpp
@@ -2,17 +2,23 @@
 #include <string>
 using namespace std;
 
-void sum_ascii(string s) {
+int sum_ascii(string s) {
     int sum=0;
+    int position = 1;
     for ( char c : s) {
-        cout << (int)c << endl;
+        cout << position << ": " << (int)c << endl;
         sum += (int)c;
+        position++;
     }
+    cout << "sum of code " << s << ": " ;
     return sum;
 }
 
 int main() {
-    cout << sum_ascii("536B9DFC93AF");
+    cout << sum_ascii("536B9DFC93AF") << endl;
+    cout << sum_ascii("1DA9D64D02A0") << endl;
+    cout << sum_ascii("666D109AA22E") << endl;
+    cout << sum_ascii("E1D2665B21EA") << endl;
     
 
     return 0;
